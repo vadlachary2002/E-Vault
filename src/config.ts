d053e0ec8ba5241d8a09e44bbb1fcc591bbad02a
vadlachary2002 <vadlanagarajuchary2002@gmail.com>
2023-12-17 16:19:01 -0800

    added goole api key

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const GOOGLE_API = 'AIzaSyCtwkEgMYgpsW35Tdh4N28sXPagOOKRakE';
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,23 +7,36 @@ import {
   useJsApiLoader,
   Marker,
   Autocomplete,
+  LoadScript,
 } from '@react-google-maps/api';
+import { GOOGLE_API } from '../../config';
 
 const Login = () => {
   const containerStyle = {
-    width: '400px',
+    width: '100%',
     height: '400px',
   };
 
   const center = {
-    lat: -3.745,
-    lng: -38.523,
+    lat: 48.745,
+    lng: 38.523,
+  };
+  const center1 = {
+    lat: 89.745,
+    lng: 80.523,
+  };
+  const center2 = {
+    lat: -12.745,
+    lng: -23.523,
+  };
+  const center3 = {
+    lat: -0.745,
+    lng: -8.523,
   };
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyCtwkEgMYgpsW35Tdh4N28sXPagOOKRakE',
-    libraries: ['places'],
   });
   const [map, setMap] = useState(null);
 
@@ -31,32 +44,45 @@ const Login = () => {
     setMap(null);
   }, []);
   return (
-    <div className="page">
-      <div className="func">
-        <Autocomplete>
-          <input type="text" placeholder="From" />
-        </Autocomplete>
-        <input type="text" name="" id="" placeholder="to" />
-        <button onClick={() => map.panTo(center)}>Center</button>
+    <ErrorBoundary>
+      <div className="page">
+        <LoadScript
+          googleMapsApiKey="AIzaSyCtwkEgMYgpsW35Tdh4N28sXPagOOKRakE"
+          libraries={['places']}
+        >
+          <div className="func">
+            <Autocomplete>
+              <input type="text" placeholder="From" />
+            </Autocomplete>
+            <Autocomplete>
+              <input type="text" name="" id="" placeholder="to" />
+            </Autocomplete>
+
+            <button onClick={() => map.panTo(center)}>Center</button>
+          </div>
+          <div className="cal">
+            {/* <button onClick={()=>map.panTo()}>Center</button> */}
+            {isLoaded ? (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={5}
+                onLoad={(map) => setMap(map)}
+                onUnmount={onUnmount}
+              >
+                {/* Child components, such as markers, info windows, etc. */}
+                {/* <Marker position={center} /> */}
+                <Marker position={center1} />
+                <Marker position={center2} />
+                <Marker position={center3} />
+              </GoogleMap>
+            ) : (
+              <></>
+            )}
+          </div>
+        </LoadScript>
       </div>
-      <div className="cal">
-        {/* <button onClick={()=>map.panTo()}>Center</button> */}
-        {isLoaded ? (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={(map) => setMap(map)}
-            onUnmount={onUnmount}
-          >
-            {/* Child components, such as markers, info windows, etc. */}
-            <Marker position={center} />
-          </GoogleMap>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
+    </ErrorBoundary>
   );
 };
 
